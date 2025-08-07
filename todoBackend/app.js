1241af3edffa6fd0d3a71e2be13fe820c0e87974
Sakshi <saks0705@gmail.com>
2025-08-07 20:57:30 +0530

    todo-apis

--- todoBackend/app.js
+++ todoBackend/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import todoRoutes from "./routes/todoRoutes.js";
 import authRoutes from "./routes/authRoutes.js"
+import { authMiddleware } from './middlewares/authMiddleware.js';
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/todos", authRoutes);
-app.use("/api/todos", todoRoutes);
+app.use("/api/todos", authMiddleware, todoRoutes);
 
 export default app;
