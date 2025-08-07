8168d74f10b02fcc8da129d58fef5a911175473b
Sakshi <saks0705@gmail.com>
2025-08-07 23:11:52 +0530

    todo-frontend-forms

---
+++ todofrontend/src/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as yup from 'yup';
+
+const LoginPage = () => {
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
