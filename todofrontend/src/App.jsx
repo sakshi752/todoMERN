7d0375b6fd734847758331e0c54b72ba57ac5817
Sakshi <saks0705@gmail.com>
2025-08-11 19:43:04 +0530

    todo-frontend-routing

--- todofrontend/src/App.jsx
+++ todofrontend/src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage/LoginPage";
+import HomePage from "./Pages/HomePage/HomePage";
+import Layout from "./Components/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<Layout />}>
+          {/* Default route inside Layout */}
+          <Route index element={<LoginPage />} />
+          {/* Example other page */}
+          <Route path="home" element={<HomePage />} />
+        </Route>
       </Routes>
     </Router>
   );
