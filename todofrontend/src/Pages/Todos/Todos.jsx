d6dd87afd846adc9c6939fd238be58b918ce27c6
Sakshi <saks0705@gmail.com>
2025-08-16 16:30:46 +0530

    frontend-todo

--- todofrontend/src/Pages/Todos/Todos.jsx
+++ todofrontend/src/Pages/Todos/Todos.jsx
@@ -1,26 +1,38 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from 'yup';
 import React, { useState } from 'react'
+import Button from "../../Components/Button";
 
 const Todos = () => {
     const [initialState, setInitialState] = useState({
         todo: "",
-        date: "",
-        // : ""
-    })
-
-
+    });
     const [validationSchema, setValidationSchema] = useState(Yup.object({
-        email: Yup.string()
-            .email("Invalid email format")
-            .required("Email is required"),
-        password: Yup.string()
-            .min(6, "Password must be at least 6 characters")
-            .required("Password is required"),
+        todo: Yup.string().required("Todo field is required"),
     }));
+
+    const handleSubmit = (values) => {
+
+    }
+
     return (
         <div>
-            <Formik>
+            <Formik
+                initialValues={initialState}
+                validationSchema={validationSchema}
+                enableReinitialize
+                onSubmit={handleSubmit}
+            >
+                <Form>
+                    <div className='flex flex-col mb-8'>
+                        <label>Todo:</label>
+                        <Field name="email" className="border p-1" />
+                        <ErrorMessage name="email" component="div" className="text-red-500" />
+                    </div>
+                    <div>
+                        <Button text={"Delete"} type={"submit"} />
+                    </div>
+                </Form>
 
             </Formik>
         </div>
