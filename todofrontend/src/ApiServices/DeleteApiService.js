9267d798135c8215c2e23c731fb60599b24fec1e
Sakshi <saks0705@gmail.com>
2025-08-20 22:27:39 +0530

    frontend-deleteService

---
+++ todofrontend/src/ApiServices/DeleteApiService.js
@@ -0,0 +1,18 @@
+import { BASE_URL } from "../utils";
+
+
+export const deleteService = async (endpoint, token) => {
+    try {
+
+        const response = await fetch(`${BASE_URL}${endpoint}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                ...(token && { Authorization: `Bearer ${token}` }),
+            }
+        })
+
+    } catch (error) {
+        console.error(error ? error : "Internal server error");
+    }
+}
